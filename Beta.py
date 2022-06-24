a36835723e46a54dd49677b97026883b68218bd5
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2022-06-25 03:07:52 +0600

    Update Beta.py

--- Beta.py
+++ Beta.py
@@ -4,8 +4,8 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from bdump import menu
+    from DUMP import menu
     menu()
 elif bit == '32bit':
-    from bdump32 import menu
+    from DUMP32 import menu
     menu()
